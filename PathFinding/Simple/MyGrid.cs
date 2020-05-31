fff4be8d0e7500e532b2dacf402fc1e64ea0dbab
Jalexy <JalexyDev@gmail.com>
2020-05-31 23:20:31 +1100

    Path Smooth Weights
    добавил сглаживание пути за счет плавного изменения веса слоя на границе с другим слоем

--- PathFinding/Simple/MyGrid.cs
+++ PathFinding/Simple/MyGrid.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PathFinding))]
+[RequireComponent(typeof(PathRequestManager))]
 public class MyGrid : MonoBehaviour
 {
     public bool displayGridGizmos;
+
+    [Header("Основные параметры")]
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
     public float nodeRadius;
+
+    [Header("Сглаживание terrainPenalty на границах маски")]
+    public bool blurPath;
+    public int blurFactor;
+
+    // увеличивает непроходимость клеток около препятствий
+    [Header("Приоритет огибания препятствий")]
     public int obstacleProximityPenalty;
+
+    [Header("Штрафы на передвижение по слоям")]
     public TerrainType[] walkableRegions;
-    LayerMask walkableMask;
-    Dictionary<int, int> walkableRegionsDictionary = new Dictionary<int, int>();
 
     Node[,] grid;
 
+    LayerMask walkableMask;
+    Dictionary<int, int> walkableRegionsDictionary = new Dictionary<int, int>();
+
     float nodeDiameter;
+    
     int gridSizeX, gridSizeY;
+
+    int penaltyMin = int.MaxValue;
+    int penaltyMax = int.MinValue;
 
     public int MaxSize
     {
@@ -31,7 +49,7 @@ public class MyGrid : MonoBehaviour
         foreach (TerrainType region in walkableRegions)
         {
             walkableMask.value |= region.terrainMask.value;
-            walkableRegionsDictionary.Add( (int) Mathf.Log(region.terrainMask.value, 2), region.terrainPennalty);
+            walkableRegionsDictionary.Add( (int) Mathf.Log(region.terrainMask.value, 2), region.terrainPenalty);
         }
 
         CreateGrid();
@@ -64,6 +82,62 @@ public class MyGrid : MonoBehaviour
                 }
 
                 grid[x, y] = new Node(walkable, worldPoint, x, y, movementPenalty);
+            }
+        }
+
+        // сглаживает углы при поиске пути
+        if (blurPath) { BlurPenaltyMap(blurFactor); }
+    }
+
+    void BlurPenaltyMap(int blurSize)
+    {
+        int kernelSize = blurSize * 2 + 1;
+        int kernelExtents = (kernelSize - 1) / 2;
+
+        int[,] penaltiesHorizontalPass = new int[gridSizeX, gridSizeY];
+        int[,] penaltiesVerticalPass = new int[gridSizeX, gridSizeY];
+
+        for (int y = 0; y < gridSizeY; y++ )
+        {
+            for (int x = -kernelExtents; x <= kernelExtents; x++)
+            {
+                int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                penaltiesHorizontalPass[0, y] += grid[sampleX, y].movementPenalty;
+            }
+
+            for (int x = 1; x < gridSizeX; x++)
+            {
+                int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX);
+                int addIndex = Mathf.Clamp(x + kernelExtents, 0, gridSizeX - 1);
+
+                penaltiesHorizontalPass[x, y] = penaltiesHorizontalPass[x - 1, y] - grid[removeIndex, y].movementPenalty + grid[addIndex, y].movementPenalty;
+            }
+        }
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = -kernelExtents; y <= kernelExtents; y++)
+            {
+                int sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                penaltiesVerticalPass[x, 0] += penaltiesHorizontalPass[x, sampleY];
+            }
+
+            int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, 0] / (kernelSize * kernelSize));
+            grid[x, 0].movementPenalty = blurredPenalty;
+
+            for (int y = 1; y < gridSizeY; y++)
+            {
+                int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY);
+                int addIndex = Mathf.Clamp(y + kernelExtents, 0, gridSizeY - 1);
+
+                penaltiesVerticalPass[x, y] = penaltiesVerticalPass[x, y - 1] - penaltiesHorizontalPass[x, removeIndex] + penaltiesHorizontalPass[x, addIndex];
+
+                blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, y] / (kernelSize * kernelSize));
+                grid[x, y].movementPenalty = blurredPenalty;
+
+                penaltyMax = blurredPenalty > penaltyMax ? blurredPenalty : penaltyMax;
+                penaltyMin = blurredPenalty < penaltyMin ? blurredPenalty : penaltyMin;
+
             }
         }
     }
@@ -111,9 +185,11 @@ public class MyGrid : MonoBehaviour
         {
             foreach (Node n in grid)
             {
-                Gizmos.color = n.walkable ? Color.white : Color.red;
+                Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(penaltyMin, penaltyMax, n.movementPenalty));
 
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+                Gizmos.color = n.walkable ? Gizmos.color : Color.red;
+
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter));
             }
         }
     }
@@ -122,6 +198,6 @@ public class MyGrid : MonoBehaviour
     public class TerrainType
     {
         public LayerMask terrainMask;
-        public int terrainPennalty;
+        public int terrainPenalty;
     }
 }
